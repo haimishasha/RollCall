3c797b990fe9f4d025458f4fea1b5e5095b57397
yanyuting <172071528@qq.com>
2015-11-01 19:22:00 +0800

    教师主页个人信息读取和修改11/1

--- routes/RollCall/teacher.js
+++ routes/RollCall/teacher.js
@@ -1,17 +1,44 @@
 var crypto = require('crypto');
 var Teacher = require('../../models/teacher/teacher.js');
 module.exports = function (app) {
-    app.get('/',checkLogin);
+    app.get('/', checkLogin);
     app.get('/', function (req, res) {
-        res.render('index', {
-            title: '教师'
+       var query ={
+           number:req.session.teacher.number
+       };
+        Teacher.get(query,function(err,teacher){
+            if(!teacher){
+                console.log(err);
+            }
+            res.render('dianming/index', {
+                title: '教师',
+                number: teacher.number,
+                trueName: teacher.trueName,
+                school: teacher.school,
+                institute: teacher.institute
+            });
         });
+
     });
     app.post('/', function (req, res) {
+        var query={
+            number:req.session.teacher.number
+        };
+        var data = req.body;
+        console.log(data);
+        Teacher.update(query,data,function(err,result){
+            if(err){
+                console.log(err);
+            }
+            console.log(result);
+            res.redirect('/');
+        });
+    });
+
+    app.get('/logout', function (req, res) {
         req.session.teacher = null;
         console.log(req.session.teacher);
         res.redirect('/login');
-
     });
     app.get('/login', function (req, res) {
         res.render('dianming/login', {
@@ -19,21 +46,21 @@ module.exports = function (app) {
         });
     });
     app.post('/login', function (req, res) {
-        Teacher.get({number:req.body.number},function(err,teacher){
-            if(!teacher){
-                req.flash('error','不存在');
+        Teacher.get({number: req.body.number}, function (err, teacher) {
+            if (!teacher) {
+                req.flash('error', '不存在');
                 console.log(err);
                 return res.redirect('/login');
             }
             var password = req.body.password;
-            if(teacher.password != password){
-                req.flash('error','密码错误');
+            if (teacher.password != password) {
+                req.flash('error', '密码错误');
                 console.log(err);
                 return res.redirect('/login');
             }
-            req.session.teacher =teacher;
+            req.session.teacher = teacher;
             console.log(req.session.teacher);
-            req.flash('success','登陆成功');
+            req.flash('success', '登陆成功');
             res.redirect('/');
         });
 
@@ -41,6 +68,8 @@ module.exports = function (app) {
     app.get('/reg', function (req, res) {
         res.render('dianming/reg', {
             title: '注册'
+
+
         });
     });
     app.post('/reg', function (req, res) {
@@ -59,37 +88,38 @@ module.exports = function (app) {
             password: req.body.password,
             institute: req.body.institute
         });
-        Teacher.get({number: newTeacher.number},function(err,teacher){
-            if(err){
-                req.flash('error',err);
+        Teacher.get({number: newTeacher.number}, function (err, teacher) {
+            if (err) {
+                req.flash('error', err);
                 console.log(err);
                 return res.redirect('/reg');
             }
-            if(teacher){
-                req.flash('error','用户存在');
-                return res.redirect('/login');
+            if (teacher) {
+                req.flash('error', '用户存在');
+                return res.redirect('/reg');
             }
-            newTeacher.save(function(err,teacher){
-                if(err){
-                    req.flash('error',err);
+            newTeacher.save(function (err, teacher) {
+                if (err) {
+                    req.flash('error', err);
                     console.log(err);
                     return res.redirect('/reg');
                 }
                 console.log(teacher);
-                req.session.teacher =teacher;
-                req.flash('success','注册成功');
+                req.session.teacher = teacher;
+                req.flash('success', '注册成功');
                 res.redirect('/login');
             });
         });
     });
-    function checkLogin(req,res,next){
-        if(req.session.teacher==null){
+    function checkLogin(req, res, next) {
+        if (req.session.teacher == null) {
             res.redirect('/login');
         }
         next();
     }
-    function checkNotLogin(req,res,next){
-        if(req.session.teacher){
+
+    function checkNotLogin(req, res, next) {
+        if (req.session.teacher) {
             res.redirect('back');
         }
         next();
