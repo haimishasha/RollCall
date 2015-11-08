fb306009b44fe3bc7ed15cfc6e7bcd99d9acef2a
shuaimeng <1174769620@qq.com>
2015-11-08 10:17:15 +0800

    教师模块完成

--- models/teacher/addCourses.js
+++ models/teacher/addCourses.js
@@ -3,25 +3,40 @@
  */
 var mongodb = require('../db');
 
-//AddCourses为数据库名 addCoursess为集合名
 var AddCourses = function (addCourses){
-    this.number = addCourses.number;//职工号
+    this.number = addCourses.number;//教师职工号
     this.courseName = addCourses.courseName;//课程名 老师上课的所有课程 
     this.courseTime = addCourses.courseTime;//课程时间 老师上课的时间
-    this.courseClass = addCourses.courseClass;//班级  老师带的所有班级
-    this.classRoom = addCourses.classRoom;//教室   
+    this.courseClass1 = addCourses.courseClass1;//班级  老师带的所有班级
+    this.courseClass2 = addCourses.courseClass2;
+    this.courseClass3 = addCourses.courseClass3;
+    this.courseClass4 = addCourses.courseClass4;
+    this.courseClass5 = addCourses.courseClass5;
+    this.courseClass6 = addCourses.courseClass6;
+    this.usernum = addCourses.usernum;//录入人数
+    this.classRoom = addCourses.classRoom;//教室
+    this.remark = addCourses.remark
 };
 module.exports = AddCourses;
 
 //存储添加菜单
 AddCourses.prototype.save = function(callback){
-    //添加课程要存入数据库的文档
-    var addCourses = {
+    var date = new Date();
+    var time = date.getFullYear()+ '-'+ date.getMonth()+ '-' +date.getDate()+ ' ' + date.getHours() + ':' + date.getMinutes() + ':' + date.getSeconds();
+    var addCoursess = {
         number:this.number,
         courseName: this.courseName,
         courseTime: this.courseTime,
-        courseClass: this.courseClass,
-        classRoom: this.classRoom
+        courseClass1:this.courseClass1,
+        courseClass2: this.courseClass2,
+        courseClass3: this.courseClass3,
+        courseClass4: this.courseClass4,
+        courseClass5: this.courseClass5,
+        courseClass6: this.courseClass6,
+        usernum: this.usernum,
+        classRoom: this.classRoom,
+        remark:this.remark,
+        time: time
     };
     //打开数据库
     mongodb.open(function(err,db){
@@ -29,69 +44,76 @@ AddCourses.prototype.save = function(callback){
             return callback(err);//错误，返回err信息
         }
         //读取addCoursess集合
-        db.collection('addCoursess',function(err,collection){
+        db.collection('addCoursess',function(err,addCourses){
             if(err){
                 mongodb.close();
                 return callback(err);//错误，返回err信息
             }
             //将添加的菜单信息插入到addCoursess集合
-            collection.insert(addCoursess,{
+            addCourses.insert(addCoursess,{
                 safe:true
-            },function(err){
+            },function(err,addCourses){
                 mongodb.close();
                 if(err){
                     return callback(err);//错误，返回err信息
                 }
-                callback(null);////返回 err 为 null
+                callback(null,addCourses);////返回 err 为 null
             });
         });
     });
 };
-
-//读取添加课程的信息
-AddCourses.get = function(num,callback){
-    //打开数据库
+AddCourses.getAll = function(query,callback){
     mongodb.open(function(err,db){
         if(err){
-            return callback(err);//错误，返回err信息
+            return callback(err);
         }
-        //读取addCoursess集合
-        db.collection('addCoursess',function(err,collection){
+        db.collection('addCoursess',function(err,addCourses){
             if(err){
                 mongodb.close();
-                return callback(err);//错误，返回err信息
+                return callback(err);
             }
-            var query = {};
-            if (name){
-                query.number = number;
-            }
-            collection.find(query).sort({}).toArray(function(err,docs){
+            addCourses.find(query).toArray(function(err,docs){
                 mongodb.close();
                 if(err){
                     return callback(err);
                 }
-                docs.forEach(function(doc){
-                    doc.post = markdown.toHTML(doc.post);
-                });
                 callback(null,docs);
             });
         });
     });
 };
-
-
-//数据库的更新操作
-AddCourses.update = function(num,data,callback){
+AddCourses.get = function(query,callback){
     mongodb.open(function(err,db){
         if(err){
             return callback(err);
         }
-        db.collection('addCourse',function(err,addCourses){
+        db.collection('addCoursess',function(err,addCourses){
             if(err){
                 mongodb.close();
                 return callback(err);
             }
-            addCourses.updata(num,{$set:data},function(err,result){
+            addCourses.insert(query,function(err,doc){
+                mongodb.close();
+                if(err){
+                    return callback(err);
+                }
+                callback(null,doc);
+            });
+        });
+    });
+};
+
+AddCourses.update = function(query,data,callback){
+    mongodb.open(function(err,db){
+        if(err){
+            return callback(err)
+        }
+        db.collection('addCoursess',function(err,addCourses){
+            if(err){
+                mongodb.close();
+                return callback(err);
+            }
+            addCourses.update(query,{$set:data},function(err,result){
                 mongodb.close();
                 if(err){
                     return callback(err);
@@ -100,25 +122,24 @@ AddCourses.update = function(num,data,callback){
             });
         });
     });
-};
+}
 
-//数据删除
-AddCourses.remove = function(num,callback){
+AddCourses.remove = function(query,callback){
     mongodb.open(function(err,db){
         if(err){
             return callback(err);
         }
-        db.collection('addCourse',function(err,addCourses){
+        db.collection('addCoursess',function(err,addCourses){
             if(err){
                 mongodb.close();
                 return callback(err);
             }
-            addCourses.remove(num,function(err,result){
+            addCourses.remove({_id:_id},{w:1},function(err){
                 mongodb.close();
                 if(err){
                     return callback(err);
                 }
-                callback(null,result);
+                callback(null);
             });
         });
     });
--- routes/RollCall/addCourses.js
+++ routes/RollCall/addCourses.js
@@ -1,18 +1,64 @@
 /**
  * Created by 11747 on 2015/10/30.
  */
+var crypto = require('crypto');
 var AddCourses = require('../../models/teacher/addCourses.js');
-
+var Teacher = require('../../models/teacher/teacher.js');
 module.exports= function(app){
+    //app.get('/Course', checkLogin);
+    app.get('/Course', function (req, res) {
+        var query ={
+            number:req.session.teacher.number
+        };
+        AddCourses.getAll(query,function(err,addCourses){
+            if(!addCourses){
+                console.log(err);
+            }
+            if(err){
+                addCourses = [];
+            }
+            res.render('dianming/Course', {
+                title:'课程',
+                addCourses:addCourses,
+                length:addCourses.length,
+            });
+        });
+    });
+    app.post('/Course', function (req, res) {
+        AddCourses.remove(function(err){
+            if(err){
+                return res.redirect('/Course');
+            }
+            res.redirect('/Course');
+        });
+    });
+    //////////////////////////////////////////////////////////////
     app.get('/addCourses', function (req, res) {
-        res.render('./dianming/addcourses',{title:'手动添加课程'});
+        var query ={
+            number:req.session.teacher.number
+        };
+        Teacher.get(query,function(err,teacher){
+            if(!teacher){
+                console.log(err);
+            }
+            res.render('./dianming/addcourses', {
+                title: '手动添加课程',
+                number: teacher.number,
+            });
+        });
     });
     app.post('/addCourses', function (req, res) {
         var newAddCourses = new AddCourses({
             number:req.body.number,
             courseName:req.body.courseName,
             courseTime:req.body.courseTime,
-            courseClass:req.body.courseClass,
+            courseClass1:req.body.courseClass1,
+            courseClass2:req.body.courseClass2,
+            courseClass3:req.body.courseClass3,
+            courseClass4:req.body.courseClass4,
+            courseClass5:req.body.courseClass5,
+            courseClass6:req.body.courseClass6,
+            usernum:req.body.usernum,
             classRoom:req.body.classRoom
         });
         newAddCourses.save(function(err){
@@ -22,7 +68,19 @@ module.exports= function(app){
             res.redirect('/course');
         });
     });
-    app.get('/course', function (req, res) {
-        res.render('./dianming/Course',{title:'课程'});
-    });
 };
+
+//    function checkLogin(req, res, next) {
+//        if (req.session.teacher == null) {
+//            res.redirect('/login');
+//        }
+//        next();
+//    }
+//
+//    function checkNotLogin(req, res, next) {
+//        if (req.session.teacher) {
+//            res.redirect('back');
+//        }
+//        next();
+//    }
+//};
